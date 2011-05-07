04f45d4ec62bea565efab67598eb9082f13bb251
Héctor Ramón Jiménez <hector0193@gmail.com>
2011-05-07 18:47:00 +0200

    Deleted example files. Deleted configuration data

--- app/views/users/show.html.php
+++
@@ -1,6 +0,0 @@
-<div class="ajax-container">
-<?php foreach($users as $user): ?>
-<p><?= $user->username ?> &raquo; <?= $user->email ?> &raquo; <?= $user->role ?></p>
-<?php endforeach; ?>
-<?= $user_pages->render(array('ajax' => true, 'pages' => 2)) ?>
-</div>
