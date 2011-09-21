b01e0b4a796ab8518bfc1e631a95203be1e126c3
Héctor Ramón Jiménez <hector0193@gmail.com>
2011-09-21 21:12:52 +0200

    [Helpers\Javascript] Added external js support.

--- helpers/Javascript.php
+++ helpers/Javascript.php
@@ -4,26 +4,46 @@ namespace Core\Helpers;
 
 class Javascript
 {
-	private $js;
+	private $local;
+	private $external;
+	private $path;
 	
 	public function __construct()
 	{
-		$this->js = array();
+		$this->local = array();
+		$this->external = array();
+		$this->path = 'http://www'. WEB_DOMAIN;
 	}
 	
-	public function add()
+	public function setPath($http)
+	{
+		$this->path = $http;
+		
+		return $this;
+	}
+	
+	public function local()
 	{
 		foreach(func_get_args() as $jsPath)
-			$this->js[] = $jsPath;
+			$this->local[] = $jsPath;
+		
+		return $this;
+	}
+	
+	public function external()
+	{
+		foreach(func_get_args() as $jsExt)
+			$this->external[] = $jsExt;
+		
+		return $this;
 	}
 	
 	public function render()
 	{
-		foreach($this->js as $jsPath)
-			$tags .= '<script type="text/javascript" src="/js/'. $jsPath .'.js"></script>'."\n";
+		foreach($this->local as $jsPath)
+			echo '<script type="text/javascript" src="'. $this->path .'/js/'. $jsPath .'.js"></script>'."\n";
 		
-		return $tags;
+		foreach($this->external as $jsExt)
+			echo '<script type="text/javascript" src="'. $jsExt .'"></script>'."\n";
 	}
 }
-
-?>
