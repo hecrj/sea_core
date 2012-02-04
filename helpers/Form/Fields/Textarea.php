35dc0221ff5d97e5adb5dbb98b46ef7308a6725f
Héctor Ramón Jiménez <hector0193@gmail.com>
2012-02-04 19:43:32 +0100

    [Core\Helpers\Form] Textarea field added

---
+++ helpers/Form/Fields/Textarea.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Helpers\Form\Fields;
+
+class Textarea extends Field {
+	static $tagName = 'textarea';
+	
+	protected $attributes = array('rows' => 15);
+	private $content;
+	
+	public function set($attribute, $value) {
+		if($attribute != 'value')
+			return parent::set($attribute, $value);
+		
+		$this->content = $value;
+		
+		return $this;
+	}
+	
+	public function getContent() {
+		return $this->content;
+	}
+}
