68db293b58cf298e035c7d4c95936e029c861bc3
Héctor <hector0193@gmail.com>
2012-07-28 18:00:31 +0200

    Added two model methods.

--- Model.php
+++ Model.php
@@ -1,9 +1,27 @@
 <?php
 
 namespace Core;
+use Core\Components\Routing\Request;
 
 class Model extends \ActiveRecord\Model
 {
+	public static function find_by($attribute, $value) {
+		$find = self::find(array($attribute => $value));
+		
+		if($find === null)
+			throw new \RuntimeException(get_called_class() .' with '. $attribute .': '. $value .' not found.');
+		
+		return $find;
+	}
 	
+	public function updateFrom(Request $request, $postKey = null, $method = 'POST') {
+		if($request->getMethod() != $method)
+			return false;
+		
+		if(null == $postKey)
+			$postKey = strtolower(get_class($this));
+			
+		return $this->update_attributes($request->post[$postKey]);
+	}
 }
 
