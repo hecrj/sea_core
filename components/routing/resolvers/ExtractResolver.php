432a8d3df9e4a9223b655d5c96941f9db0a78d7b
Héctor <hector0193@gmail.com>
2012-09-03 17:40:26 +0200

    [ExtractResolver] Default actions redirection.

--- components/routing/resolvers/ExtractResolver.php
+++ components/routing/resolvers/ExtractResolver.php
@@ -8,11 +8,24 @@ class ExtractResolver extends ResolverAbstract
 {	
 	public function getContext(RequestInterface $request, RouteCollectionInterface $routes)
 	{	
-		$arguments = explode('/', substr($request->getPath(), 1));
+		$path = $request->getPath();
+
+		if($path == '/index')
+			$request->redirectTo('/');
+
+		$arguments = explode('/', substr($path, 1));
+
 		$controller = array_shift($arguments);
 
 		$controllerName = $controller ? ucfirst($controller) : 'Index';
-		$actionName = array_shift($arguments) ?: 'index';
+		
+		$actionName = array_shift($arguments);
+
+		if($actionName == 'index')
+			$request->redirectTo("/$controller");
+		
+		if(empty($actionName))
+			$actionName = 'index';
 
 		$contextClass = $this->contextClass;
 		return new $contextClass($controllerName, $actionName, $arguments, $routes->getModuleName());
